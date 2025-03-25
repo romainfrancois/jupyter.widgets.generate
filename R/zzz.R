c8e4879d36f5423612061727f7b98dbd7847bba4
Romain Francois <romain@tada.science>
2025-03-25 12:43:41 +0100

    import / export

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,6 @@
 #' @import dplyr
+#' @importFrom purrr map2
+#' @importFrom rlang current_env check_dots_empty caller_env arg_match is_true list2 caller_arg
+#' @importFrom glue glue glue_collapse
+#' @importFrom cli cli_abort
 NULL
