089875cbe0e52cb70806cd9534bb510eea99683b
Romain Francois <romain@tada.science>
2025-03-25 14:07:36 +0100

    + generate_style_widget()

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 #' @import dplyr
-#' @importFrom purrr map2
+#' @importFrom purrr map2 map2_chr
 #' @importFrom rlang current_env check_dots_empty caller_env arg_match is_true list2 caller_arg
 #' @importFrom glue glue glue_collapse
 #' @importFrom cli cli_abort
