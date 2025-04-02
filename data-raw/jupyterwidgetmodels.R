304e163c04574d293d65a8f928b2c4a287cc248c
Romain Francois <romain@tada.science>
2025-04-02 09:10:48 +0200

    include enum in jupyterwidgetmodels

--- data-raw/jupyterwidgetmodels.R
+++ data-raw/jupyterwidgetmodels.R
@@ -17,20 +17,21 @@ view <- map_dfr(models, \(x) {
 
 model <- map_dfr(models, \(x) {
   tibble(
-    `_model_module` = x$model$module,
+    `_model_module`         = x$model$module,
     `_model_module_version` = x$model$version,
-    `_model_name` = if (is.null(x$model$name)) "null" else x$model$name
+    `_model_name`           = if (is.null(x$model$name)) "null" else x$model$name
   )
 })
 
 attributes <- map(models, \(x) {
   out <- map_dfr(x$attributes, \(attr) {
     tibble(
-      name = attr$name,
-      type = list(attr$type),
-      help = attr$help,
-      default = list(attr$default),
-      allow_none = attr$allow_none %||% TRUE
+      name       = attr$name,
+      type       = list(attr$type),
+      help       = attr$help,
+      default    = list(attr$default),
+      allow_none = attr$allow_none %||% TRUE,
+      enum       = list(as.character(attr$enum))
     )
   })
 
